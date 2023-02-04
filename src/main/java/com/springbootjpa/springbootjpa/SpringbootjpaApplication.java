6672baaef9421eee21962418def452af17bbfb59
onidino <lucianodefilippe@gmail.com>
2023-02-04 15:56:32 -0300

    First Commit for SpringBoot JPA learning app

---
+++ src/main/java/com/springbootjpa/springbootjpa/SpringbootjpaApplication.java
@@ -0,0 +1,13 @@
+package com.springbootjpa.springbootjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootjpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootjpaApplication.class, args);
+	}
+
+}
