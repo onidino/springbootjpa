5b9a33ef75770494f443d3287bce557c8afa55fc
onidino <lucianodefilippe@gmail.com>
2023-02-12 21:06:11 -0300

    exploring more functions of Spring Data JPA

--- src/main/java/com/springbootjpa/springbootjpa/repository/CourseCommandLineRunner.java
+++ src/main/java/com/springbootjpa/springbootjpa/repository/CourseCommandLineRunner.java
@@ -34,23 +34,30 @@ public class CourseCommandLineRunner implements CommandLineRunner {
 
   @Override
   public void run(String... args) {
-    // Testing inserting rows in the h2 db
-    LOG.info("INSERTING ROWS 1, 2, 3");
+    LOG.info("Testing inserting rows in the h2 db");
 // repository.insert is only for JDBC & JPA repositories
 //    repository.insert(new Course(1, "LEARN SPRING BOOT", "in28minutes"));
 //    repository.insert(new Course(2, "LEARN AWS", "in28minutes"));
 //    repository.insert(new Course(3, "LEARN GRAPHQL", "in28minutes"));
     repository.save(new Course(1, "LEARN SPRING BOOT", "in28minutes"));
     repository.save(new Course(2, "LEARN AWS", "in28minutes"));
-    repository.save(new Course(3, "LEARN GRAPHQL", "in28minutes"));
+    repository.save(new Course(3, "LEARN GRAPHQL", "testAuthor"));
 
-    // Testing deleting row with id 2
-    LOG.info("DELETING ROW 2");
+    LOG.info("Testing listing all rows");
+    LOG.info("ROWS: COUNT {} - {}", repository.count(), repository.findAll());
+
+    LOG.info("Testing deleting row with id 2");
     repository.deleteById(2L);
+    LOG.info("ROWS: COUNT {} - {}", repository.count(), repository.findAll());
 
-    // Testing find a row by id
-    LOG.info("FINDING ROW 1, 3");
-    LOG.info("ROW 1 {}", repository.findById(1L));
-    LOG.info("ROW 3 {}", repository.findById(3L));
+    LOG.info("Testing find a row by id 1, 3");
+    LOG.info("ROW 1: {}", repository.findById(1L));
+    LOG.info("ROW 3: {}", repository.findById(3L));
+
+    LOG.info("Testing find a row by author: 'testAuthor'");
+    LOG.info("ROW: {}", repository.findByAuthor("testAuthor"));
+
+    LOG.info("Testing find a row by name: 'LEARN SPRING BOOT'");
+    LOG.info("ROW: {}", repository.findByName("LEARN SPRING BOOT"));
   }
 }
